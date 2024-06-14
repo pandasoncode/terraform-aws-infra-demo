b6713d151049e3d8992ccfa81345c5fba7d1dd9b
Fernando Nicolas Carrillo Lopez <pandasoncode@gmail.com>
2024-06-14 12:38:18 +0200

    feat: Add nullable and sensitive flags to VPC variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,15 +1,20 @@
 variable "vpc_name" {
   description = "The name of the VPC"
+  nullable    = false
+  sensitive   = false
   type        = string
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
+  nullable    = false
+  sensitive   = false
   type        = string
 }
 
 variable "availability_zones" {
   description = "The availability zones for the VPC"
+  nullable    = false
+  sensitive   = false
   type        = list(string)
 }
-
