fa664b77ae216f44b5b62d99767fd605a780e244
Fernando Nicolas Carrillo Lopez <pandasoncode@gmail.com>
2024-06-17 18:23:26 +0200

    chore: Remove unused AWS credentials variables and update default environment value

--- variables.tf
+++ variables.tf
@@ -1,24 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  nullable    = false
-  sensitive   = false
-  type        = string
-}
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  nullable    = false
-  sensitive   = true
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret access key"
-  nullable    = false
-  sensitive   = true
-  type        = string
-}
-
 variable "environment" {
   default     = "development"
   description = "Environment"
